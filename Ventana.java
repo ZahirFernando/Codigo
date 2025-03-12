a46c428cdb17262625917f96658a5b9a68980833
ZahirFernando <zdiaz_23@alu.uabcs.mx>
2025-03-12 15:35:05 -0700

    unidad 2 ejercicio 33.

--- Ventana.java
+++ Ventana.java
@@ -38,7 +38,7 @@ public class Ventana extends JFrame {
 
    public Ventana() {	//commit login
         this.setVisible(true);
-        this.setSize(1500, 1000);
+        this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setTitle("Login");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -587,7 +587,159 @@ public class Ventana extends JFrame {
   		Graphics2D g2d = (Graphics2D) g.create ();
   		g2d.setStroke(new BasicStroke(5));
   		
-  		g2d.setColor(Color.decode("#5ED3E0"));
+  		g2d.setColor(Color.decode("#413CC3"));
+        g2d.fillRect(0, 0, 800, 600);
+        
+        g2d.setColor(Color.BLACK);
+		g2d.setStroke(new BasicStroke(10));
+		g2d.drawRect(0, 500, 1300, 100);
+		
+		g2d.setColor(Color.decode("#AF8260"));
+		g2d.fillRect(0, 500, 1300, 100);
+		
+		g2d.setColor(Color.WHITE);
+		g2d.fillRoundRect(40, 70, 140, 30, 50, 50);
+		g2d.fillRoundRect(40, 110, 190, 30, 50, 50);
+		g2d.fillRoundRect(300, 50, 190, 30, 50, 50);
+		g2d.fillRoundRect(230, 200, 100, 30, 50, 50);
+		g2d.fillRoundRect(180, 300, 100, 30, 50, 50);
+		g2d.fillOval(100, 80, 10, 10);
+		
+		g2d.setColor(Color.decode("#A9B5DF"));
+		g2d.fillRect(75, 225, 150, 270);
+		g2d.fillOval(75, 170, 150, 110);
+		
+		
+		
+		g2d.setColor(Color.decode("#0C84F5"));
+		g2d.fillRect(-50, 396, 200, 100);
+		g2d.fillOval(-50, 345, 200, 100);
+		
+		g2d.setColor(Color.decode("#A9B5DF"));
+		g2d.fillRect(300, 225, 150, 270);
+		g2d.fillOval(300, 170, 150, 110);
+		
+		g2d.fillRect(450, 145, 150, 350);
+		g2d.fillOval(450, 95, 150, 110);
+		
+		g2d.setColor(Color.WHITE);
+		g2d.fillRoundRect(350, 250, 300, 30, 50, 50);
+		g2d.fillRoundRect(550, 200, 200, 30, 50, 50);
+		
+		g2d.setColor(Color.decode("#0C84F5"));
+		g2d.fillRect(300, 396, 165, 100);
+		g2d.fillOval(300, 345, 165, 100);
+		
+		g2d.fillRect(475, 250, 200, 246);
+		g2d.fillOval(475, 200, 200, 100);
+		
+		g2d.setColor(Color.BLACK);
+		g2d.setStroke(new BasicStroke(7));
+		g2d.drawRect(600, 445, 50, 50);
+		g2d.drawRect(600, 390, 50, 50);
+		g2d.drawRect(600, 335, 50, 50);
+		g2d.drawRect(600, 280, 50, 50);
+		g2d.drawRect(654, 280, 50, 50);
+		g2d.drawRect(707, 280, 50, 50);
+		g2d.drawRect(760, 280, 50, 50);
+		g2d.drawRect(700, 415, 140, 80);
+		g2d.fillRect(690, 414, 140, 85);
+		g2d.drawRect(680, 350, 160, 62);
+
+		g2d.drawRect(300, 370, 130, 125);
+		g2d.fillRect(310, 370, 130, 130);
+		g2d.drawRoundRect(285, 300, 160, 65, 20, 20);
+		g2d.fillRoundRect(295, 300, 160, 75, 20, 20);
+		
+		
+		g2d.setColor(Color.decode("#9AA6B2"));
+		g2d.fillRect(600, 445, 50, 50);
+		g2d.fillRect(600, 390, 50, 50);
+		g2d.fillRect(600, 335, 50, 50);
+		g2d.fillRect(600, 280, 50, 50);
+		
+		g2d.setColor(Color.yellow);
+		g2d.fillRect(654, 280, 50, 50);
+		g2d.fillRect(707, 280, 50, 50);
+		g2d.fillRect(760, 280, 50, 50);
+		
+		g2d.setColor(Color.decode("#A0C878"));
+		g2d.fillRect(700, 415, 140, 80);
+		g2d.fillRect(680, 350, 160, 62);
+		g2d.fillRect(300, 370, 130, 125);
+		g2d.fillRoundRect(285, 300, 160, 65, 20, 20);
+		
+		g2d.setColor(Color.decode("#A0C878"));
+		g2d.fillRect(410, 375, 20, 120);
+		g2d.fillRoundRect(425, 300, 20, 65, 5, 5);
+		
+		
+		g2d.setColor(Color.decode("#A0C878"));
+		g2d.fillRect(695, 415, 10, 80);
+		g2d.fillRect(680, 355, 10, 57);
+		g2d.setColor(Color.BLACK);
+		g2d.fillRoundRect(290, 365, 145, 10, 10, 10);
+
+		try {
+			BufferedImage image = ImageIO.read(new File("images/mario.2.png"));
+			g2d.drawImage(image, 50, 355, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			BufferedImage image = ImageIO.read(new File("images/planta5.png"));
+			g2d.drawImage(image, 290, 220, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+				
+		g2d.setColor(Color.BLACK);
+		g2d.drawRect(0, 500, 800, 40);
+		g2d.setColor(Color.green);
+		g2d.fillRect(0, 500, 800, 40);
+		
+	}
+	
+}	
+
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		
+  		/*g2d.setColor(Color.decode("#5ED3E0"));
   		g2d.drawRect(0, 0, 1500, 1000);
   		g2d.fillRect(0, 0, 1500, 1000);
   		
@@ -971,8 +1123,8 @@ public class Ventana extends JFrame {
 
 	  	
   	
-  	}
-}
+  	
+
 
 
 
